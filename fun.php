f05b63cc3bb7218896df877de6fce360fd06e5b1
Aleksei Prokopev <1641387@mail.ru>
2022-07-19 15:30:37 +0800

    Add files via upload

---
+++ fun.php
@@ -0,0 +1,22 @@
+<?php 
+// include_once('function.php');
+function  getArticles(){
+return  [
+        '1' => [
+            'id' => 1,
+            'title' => 'Hello Wordl!', 
+            'content' => 'Hello all worlds! Some text.'
+        ],
+        '2' => [
+            'id' => 2,
+            'title' => 'Other article!',
+            'content' => 'Some text about other.'
+        ],
+        '5' => [
+            'id' => 5,
+            'title' => 'Other article!',
+            'content' => 'Some text about one more.'
+        ],
+        
+    ];
+}
